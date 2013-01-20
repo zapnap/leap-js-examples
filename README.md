eef178c95954036c7d32abd4c9221bb41adae9eb
Nick Plante <nap@zerosum.org>
2013-01-20 13:06:25 -0500

    update + full-screen canvas example

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ Some JavaScript / Canvas examples for Leap Motion
 
 http://developer.leapmotion.com/
 
-NOTE: you need to have the LEAP SDK / WebSocket server installed (SDK version 0.6.6+)
+NOTE: you need to have the LEAP SDK / WebSocket server installed (SDK version 0.7.1+)
--- leapcanvas.js
+++ leapcanvas.js
@@ -3,10 +3,21 @@ var LeapEx = {
   ctx: null,
   offsetX: 250,
   hackY: 500,
+  width: null,
+  height: null,
+  debugEl: null,
   el: null,
+  leapMinX: -200,
+  leapMaxX: 200,
+  leapMinY: 100,
+  leapMaxY: 600,
+  leapMinZ: -180,
+  leapMaxZ: 180,
+  started: false,
 
-  init: function(el) {
+  init: function(el, debugEl) {
     LeapEx.el = $(el);
+    LeapEx.debugEl = $(debugEl);
 
     // Support both the WebSocket and MozWebSocket objects
     if ((typeof(WebSocket) == 'undefined') &&
@@ -14,46 +25,79 @@ var LeapEx = {
       WebSocket = MozWebSocket;
     }
 
+    var w = LeapEx.width = $(window).width();
+    var h = LeapEx.height = $(window).height();
+    $(el).attr('width', w).css('width', w).attr('height', h).css('height', h);
+    $(el).css('position', 'absolute').css('left', '0').css('top', '0');
+
     LeapEx.ctx = $(el)[0].getContext("2d");
     LeapEx.ws = new WebSocket("ws://localhost:6437/");
 
     LeapEx.ws.onopen = function(event) {
-      document.getElementById("connection").innerHTML = "WebSocket connection open!";
+      LeapEx.debug("WebSocket connection open!");
     };
 
     LeapEx.ws.onclose = function(event) {
       LeapEx.ws = null;
-      document.getElementById("connection").innerHTML = "WebSocket connection closed";
+      LeapEx.debug("WebSocket connection closed");
     };
 
     LeapEx.ws.onerror = function(event) {
-      console.log("Received error");
+      LeapEx.debug("Received error");
     };
 
     LeapEx.ws.onmessage = function(event) {
-      var obj = JSON.parse(event.data);
-      var str = JSON.stringify(obj, undefined, 2);
-      // console.log(str);
+      if (LeapEx.started) {
+        var obj = JSON.parse(event.data);
+        var str = JSON.stringify(obj, undefined, 2);
 
-      if (obj.hands.length > 0) {
-        var hand = obj.hands[0];
-        var x = hand.palmPosition[0];
-        var y = hand.palmPosition[1];
-        var z = hand.palmPosition[2];
+        LeapEx.debug(str);
 
-        if (z < 0) { z = 0; }
-        LeapEx.draw(x, y, z);
+        if (typeof(obj.hands) != 'undefined' && obj.hands.length > 0) {
+          var hand = obj.hands[0];
+          var x = hand.palmPosition[0];
+          var y = hand.palmPosition[1];
+          var z = hand.palmPosition[2];
+
+          if (z < 0) { z = 0; }
+          LeapEx.draw(x, y, z);
+        }
       }
     };
 
+    $(document.body).click(function() {
+      LeapEx.toggle();
+    });
+
+    LeapEx.started = true;
     return LeapEx.el;
   },
 
   draw: function (x, y, z) {
-    LeapEx.ctx.clearRect(0,0,500,500);
+    LeapEx.ctx.clearRect(0,0,LeapEx.width,LeapEx.height);
     LeapEx.ctx.beginPath();
-    LeapEx.ctx.arc(x + LeapEx.offsetX, LeapEx.hackY - y, z, 0, Math.PI*2, true);
+    LeapEx.ctx.arc(LeapEx.scale(x, LeapEx.leapMinX, LeapEx.leapMaxX, -100, LeapEx.width),
+                   LeapEx.scale(y, LeapEx.leapMinY, LeapEx.leapMaxY, LeapEx.height, -100),
+                   z, 0, Math.PI*2, true);
     LeapEx.ctx.closePath();
     LeapEx.ctx.fill();
+  },
+
+  scale: function(value, oldMin, oldMax, newMin, newMax) {
+    return (((newMax - newMin) * (value - oldMin)) / (oldMax - oldMin)) + newMin;
+  },
+
+  toggle: function() {
+    if (LeapEx.started) {
+      LeapEx.started = false;
+    } else {
+      LeapEx.started = true;
+    }
+  },
+
+  debug: function(message) {
+    if (LeapEx.debugEl) {
+      LeapEx.debugEl.text(message);
+    }
   }
 };
